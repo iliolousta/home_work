062fb31f4923f58e61cda8c5f565b656d2766cb7
iliolousta <panfilowa.polina15@gmail.com>
2025-04-12 12:14:13 +0300

    Update 2_hw.py

--- 2_hw.py
+++ 2_hw.py
@@ -1,1 +1,7 @@
+#1
+def task_1(a: int, b: float, c: str, d: list, e: bool):
+    for elem in task_1:
+        return isinstance()
+
+print(type(3), type(9.2), type("hello"), type([5, "May", True]), type(False))
 
